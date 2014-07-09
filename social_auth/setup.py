5b7288da08727c77ff071f7b33f95d99a2f8aa8d
codeware2014 <wahab@codewaretecchnologies.com>
2014-07-09 16:41:39 +0200

    JSonSerializeSupport
    patch1

---
+++ social_auth/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from distutils.core import Command
+
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from django.conf import settings
+        settings.configure(DATABASES={'default': {'NAME': ':memory:',
+            'ENGINE': 'django.db.backends.sqlite3'}},
+            INSTALLED_APPS=('jsonserializepatch',))
+        from django.core.management import call_command
+        call_command('test', 'jsonserializepatch')
+
+
+setup(name='jsonserializepatch',
+    version='1',
+    packages=['jsonfield'],
+    license='BSD',
+    author='patch',
+    author_email='codeware@gmail.com',
+    url='https://github.com/codeware2014/jsonserializepatch/',
+    description='A reusable Django field that allows you to store validated JSON in your model.',
+    long_description=open("README.rst").read(),
+    install_requires=['Django >= 1.4.3'],
+    tests_require=['Django >= 1.4.3'],
+    cmdclass={'test': TestCommand},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+    ],
+)
